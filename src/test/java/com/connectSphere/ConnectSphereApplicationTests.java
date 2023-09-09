5354f95346992db6725cf321e398a230f91c4cd5
UditNova <bemegamale@gmail>
2023-09-09 09:22:18 +0530

    Project setup

---
+++ src/test/java/com/connectSphere/ConnectSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
